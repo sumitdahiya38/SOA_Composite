fba0245323380b4b148ced12fd78ce45b060d85c
sumitdahiya38 <sumitdahiya38@gmail.com>
2018-02-09 13:02:14 +0530

    deleted some files

--- weblogic.py
+++
@@ -1,3 +0,0 @@
-connect("weblogic","weblogic123","t3://172.17.17.115:7001")
-sca_deployComposite("http://172.17.17.115:7004","C:\Users\sumit\Desktop\git\SOA_Composite\MyFirstApp\deploy\sca_MyFirstApp1.jar",user="weblogic",password="weblogic123",overwrite=true)
-
