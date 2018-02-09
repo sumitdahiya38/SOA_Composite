a3e48359b0c1e195eae533453d66fe1f0fb83f87
sumitdahiya38 <sumitdahiya38@gmail.com>
2018-02-09 12:23:58 +0530

    applying changes

--- weblogic.py
+++ weblogic.py
@@ -1,2 +1,3 @@
 connect("weblogic","weblogic123","t3://172.17.17.115:7001")
-sca_deployComposite("http://172.17.17.115:7004","SOA_Composite/blob/master/MyFirstApp/deploy/sca_MyFirstApp1.jar",user="weblogic",password="weblogic123",overwrite=true)
+sca_deployComposite("http://172.17.17.115:7004","C:\Users\sumit\Desktop\git\SOA_Composite\MyFirstApp\deploy\sca_MyFirstApp1.jar",user="weblogic",password="weblogic123",overwrite=true)
+
